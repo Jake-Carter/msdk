76af84d62e9a8ad5b51089224dc75d61b000d7fb
Jake-Carter <Jake-Carter@users.noreply.github.com>
2023-07-27 18:45:01 +0000

    Deploying to gh-pages from @ Jake-Carter/msdk@8f201197d11b4f47f14daab45d8801b688fc0935 🚀

--- Libraries/PeriphDrivers/Documentation/MAX78002/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['extclk_5ffreq_0',['EXTCLK_FREQ',['../system__max78002_8h.html#a8ea36a37123f9910f214369c3d158282',1,'system_max78002.h']]]
-];
--- Libraries/PeriphDrivers/Documentation/MAX78002/search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['peripheralclock_0',['PeripheralClock',['../system__max78002_8h.html#a533988e986ca329348027c7dd274028d',1,'system_max78002.h']]]
-];
